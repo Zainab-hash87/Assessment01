28d8589171f0f69faed13df3f854fc7422095b19
Zaynab Afzal <zainabafzal8888@gmail.com>
2024-11-24 20:48:51 +0400

    Add files via upload

---
+++ ASSESMENT 01CODING/Ex04.py
@@ -0,0 +1,22 @@
+def quiz():
+    capitals = {
+        "France": "Paris",
+        "Germany": "Berlin",
+        "Italy": "Rome",
+        "Spain": "Madrid",
+        "Netherlands": "Amsterdam",
+        "Belgium": "Brussels",
+        "Sweden": "Stockholm",
+        "Norway": "Oslo",
+        "Finland": "Helsinki",
+        "Austria": "Vienna"
+    }
+
+    for country, capital in capitals.items():
+        answer = input(f"What is the capital of {country}? ")
+        
+        if answer.lower() == capital.lower():
+            print("Right!")
+        else:
+            print(f"Wrong! The correct answer is {capital}.")
+quiz()
---
+++ ASSESMENT 01CODING/Ex08.py
@@ -0,0 +1,8 @@
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+search_name = input("Enter the name you are looking for: ")
+
+if search_name in names:
+    print(f"{search_name} It is in the list.")
+else:
+    print(f"{search_name} It is not found in the list.")
