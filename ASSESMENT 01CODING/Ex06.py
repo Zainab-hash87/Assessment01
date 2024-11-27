4bb19e7a6ef9ba1f9fc7ed335e9abf286ca59bc2
Zaynab Afzal <zainabafzal8888@gmail.com>
2024-11-27 19:54:17 +0400

    Update Ex06.py

--- ASSESMENT 01CODING/Ex06.py
+++ ASSESMENT 01CODING/Ex06.py
@@ -1,16 +1,18 @@
+#declaring the correct password
 correct_password = "12345"
 max_attempts = 5
-attempts = 0
+attempts = 0      #initial attempts 
 
 while attempts < max_attempts:
-    password = input("Enter the password: ")
+    password = input("Enter the password: ")     #asking user to enter their password
     
+#using if else statement to check the password 
     if password == correct_password:
         print("Signed in!")
         break
     else:
         attempts += 1
-        remaining_attempts = max_attempts - attempts
+        remaining_attempts = max_attempts - attempts    #this will display the remaining attempts left
         if remaining_attempts > 0:
             print(f"Incorrect password. You have {remaining_attempts} attempts left.")
         else:
