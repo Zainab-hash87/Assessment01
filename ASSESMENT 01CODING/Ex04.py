855019c2ba8bd6afdd3b538bc3be489ae3f4b815
Zaynab Afzal <zainabafzal8888@gmail.com>
2024-11-27 19:45:59 +0400

    Update Ex04.py

--- ASSESMENT 01CODING/Ex04.py
+++ ASSESMENT 01CODING/Ex04.py
@@ -1,3 +1,4 @@
+#using def function to create a small quiz
 def quiz():
     capitals = {
         "France": "Paris",
@@ -11,12 +12,13 @@ def quiz():
         "Finland": "Helsinki",
         "Austria": "Vienna"
     }
-
+#using for function 
     for country, capital in capitals.items():
         answer = input(f"What is the capital of {country}? ")
-        
+#usig if else statement to check the answer     
         if answer.lower() == capital.lower():
             print("Right!")
         else:
             print(f"Wrong! The correct answer is {capital}.")
+    #calling the function 
 quiz()
