337ec667c4320f25a99c06768d728362348abc81
Zaynab Afzal <zainabafzal8888@gmail.com>
2024-11-27 19:59:31 +0400

    Update Ex08.py

--- ASSESMENT 01CODING/Ex08.py
+++ ASSESMENT 01CODING/Ex08.py
@@ -1,7 +1,10 @@
+#creating a list of names
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
+#asking user to enter a name they are looking for
 search_name = input("Enter the name you are looking for: ")
 
+#using if else statement to verify the user's input
 if search_name in names:
     print(f"{search_name} It is in the list.")
 else:
